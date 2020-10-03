a270cd3402504069233a11dfb5287fd33d3dcb69
Gaby G <paogaby0803@gmail.com>
2020-10-03 17:12:32 -0500

    [8] Adjust css according to the design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,24 @@
 import React from 'react'
+import './header.css'
 import Nav from 'react-bootstrap/Nav'
 
 export default function Header() {
     return (
-        <Nav
+        <Nav className="header"
         activeKey="/home"
         onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
       >
         <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
+          <Nav.Link className="item-logo" href="/home">Pettnaut</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
+          <Nav.Link className="item" eventKey="link-1">Acerca de</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
+          <Nav.Link className="item" eventKey="link-2">Colaborar</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
+          <Nav.Link className="item" eventKey="link-2">Sponsors</Nav.Link>
         </Nav.Item>
       </Nav>
     )
